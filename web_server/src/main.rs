fdcf1c973fa7e019587dc07d1edf406c566f4e45
annyedge <annyedge@tutamail.com>
2024-08-31 15:25:37 +0200

    refactor

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -14,27 +14,22 @@ fn main() {
     }
 }
 
-// --snip--
-
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("./util/hello.html").unwrap();
-        let length = contents.len();
+    let (status_line, file_path) = match request_line.as_str() {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "./util/hello.html"),
+        _ => ("HTTP/1.1 404 NOT FOUND", "./util/404.html"),
+    };
 
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response = build_response(status_line, file_path);
+    stream.write_all(response.as_bytes()).unwrap();
+}
 
-        stream.write_all(response.as_bytes()).unwrap();
-    } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("./util/404.html").unwrap();
-        let length = contents.len();
+fn build_response(status_line: &str, file_path: &str) -> String {
+    let contents = fs::read_to_string(file_path).unwrap();
+    let length = contents.len();
 
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-        stream.write_all(response.as_bytes()).unwrap();
-    }
+    format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}")
 }
